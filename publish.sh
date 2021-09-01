6a108d96616b5c479d9b98948f499ec78c6a1b38
ErikXu <81636759@qq.com>
2021-09-01 10:39:42 +0000

    Add publish script.

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf ./publish
+mkdir -p ./publish
+
+rustup target add wasm32-wasi
+cargo install wasm-gc
+
+cargo build --target wasm32-wasi --release --target-dir ./publish
+
+# 压缩 wasm
+wasm-gc ./publish/wasm32-wasi/release/envoy-filter.wasm
