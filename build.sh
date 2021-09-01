616c70a31315a89b9b412a9a016bfa06357f739f
ErikXu <81636759@qq.com>
2021-09-01 10:55:29 +0000

    Add build script.

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run --rm -i \
+    -v /usr/local/cargo/registry:/usr/local/cargo/registry \
+    -v ${PWD}:/workspace \
+    rust:1.54 \
+    sh -c 'cd /workspace && sh publish.sh'
--- publish.sh
+++ publish.sh
@@ -9,4 +9,4 @@ cargo install wasm-gc
 cargo build --target wasm32-wasi --release --target-dir ./publish
 
 # 压缩 wasm
-wasm-gc ./publish/wasm32-wasi/release/envoy-filter.wasm
+wasm-gc ./publish/wasm32-wasi/release/envoy_filter.wasm
